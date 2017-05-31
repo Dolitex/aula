dcbc66e9571a9676e6336c7e27700ef7f621c74a
˜Lucas˜ <rianmedicobf@hotmail.com>
2017-05-30 23:28:32 -0300

    Base do programa

--- sync.c
+++ sync.c
@@ -1,1 +1,21 @@
-Haeaheae
+#include <stdio.h>
+
+Int main()
+{
+	char comando;
+	int n1,n2,result;
+
+	printf(“Digite a operação que deseja fazer”);
+	scanf(“%c”, &comando);
+
+	printf(“Digite os dois números da operação”);
+	scanf(“%d %d”, &n1, &n2);
+
+	if(comando == ‘+’)
+	{
+		result = n1 + n2;
+		printf(“%d”, result);
+	}	
+
+	return 0;
+}
